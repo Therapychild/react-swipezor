5d75d0bc72224ba774324a0ac7ea7df2804fc399
Jeremy Antoine <jeremy.antoine@antoinesolutions.com>
2021-04-29 09:21:42 -0700

    Updated the Type for the onSwipeDone prop from "function" to "Function".

--- src/index.d.ts
+++ src/index.d.ts
@@ -2,14 +2,14 @@ import * as React from "react";
 interface SwipezorProps {
   mainText: any;
   overlayText: any;
-  onSwipeDone: function;
+  onSwipeDone: Function;
   reset?: Number;
   classList?: String;
   overlayClassList?: String;
   caretClassList?: String;
   delta?: Number;
   minSwipeWidth?: Number;
-  minSwipeVelocity?: Number;  
+  minSwipeVelocity?: Number;
   caret?: any;
 }
 declare class Swipezor extends React.Component<SwipezorProps, any> {}
